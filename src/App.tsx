8e8c35f9e1219393802d55b8899e477299b390bd
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-10 22:35:25 +0600

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-green-400">
+        dev stack started.......
+      </h1>
+    </>
+  );
+}
+
+export default App;
